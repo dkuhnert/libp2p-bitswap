25e7881cd9031b5bf302bb9a3e6560cb2aa40940
dkuhnert <32337621+dkuhnert@users.noreply.github.com>
2024-06-12 10:53:53 +0200

    build: added protobuf build

---
+++ build.rs
@@ -0,0 +1,12 @@
+use pb_rs::{types::FileDescriptor, ConfigBuilder};
+
+fn main() {
+    let in_files: Vec<&str> = vec![
+        "src/compat/pb/bitswap_pb.proto",
+    ];
+    for in_file in in_files.iter() {
+        println!("cargo:rerun-if-changed={}", in_file);
+    }
+    let config_builder = ConfigBuilder::new(&in_files, None, None, &[]).unwrap();
+    FileDescriptor::run(&config_builder.build()).unwrap()
+}
